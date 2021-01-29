85f55e139157701e59b8bc35321177f4a55bbb53
yuuki-abc <yuki.abc.300@gmail.com>
2021-01-29 16:45:24 +0000

    html,css調整中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,25 +19,33 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book), method: :get
+    redirect_to book_path(book)
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path, method: :get
+    @book = Book.new(book_params)
+    @books = Book.all
+    # ここでインスタンス変数で定義しておけば、render先でこの変数を使える。
+
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render action: :books
+      # createアクション内で定義したインスタンス変数でbooks viewを開ける
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path, method: :get
+    redirect_to books_path
   end
 
   private
 
   def book_params
     # 命名規則は、モデル名_paramsが一般的。
+    # paramsメソッドにはフォームから送られてきたデータが入る。
     params.require(:book).permit(:title, :body)
   end
 
