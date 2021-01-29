70a50fb587c3691b830896409f07668da8b048f8
yuuki-abc <yuki.abc.300@gmail.com>
2021-01-29 04:30:24 +0000

    topページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'books/top'
-  get 'books/books'
+  get '/' => 'books#top'
+  get '/books' => 'books#books'
   get 'books/show'
   get 'books/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
