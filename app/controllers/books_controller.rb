8aa3cb392a4b79878a20ab885210f700da374f89
yuuki-abc <yuki.abc.300@gmail.com>
2021-01-30 18:28:26 +0000

    完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,6 +20,9 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book), notice: "Editing was successfully"
+    # redirect_toメソッドは引数にキーと値のセットを入れればflash連想配列へ、
+    # 格納してくれる。
+    # noticeとalertというキーに関しては、{}を省略して入力出来る
   end
 
   def create
@@ -28,9 +31,11 @@ class BooksController < ApplicationController
     # ここでインスタンス変数で定義しておけば、render先でこの変数を使える。
 
     if @book.save
+      # saveメソッドはセーブに失敗した時に@bookにエラーの内容等を配列として
+      # 全て格納してくれるのか？
       redirect_to book_path(@book), notice: "Created successfully"
     else
-      flash[:alert] = "error"
+      # flash[:alert] = "error" # エラー表示が多くクドいかも
       # flashという名前の連想配列のnoticeというキーの値に、メッセージを代入
       render action: :books
       # createアクション内で定義したインスタンス変数でbooks viewを開ける
